f8ac541c538c2641bd04e4cb0e64979560162fc9
Abhijeet Prasad <AbhiPrasad@users.noreply.github.com>
2020-05-22 10:39:09 -0400

    feat: Convert OTel Spans to Sentry Spans (#3)

--- exporter/sentryexporter/sentry_exporter.go
+++ exporter/sentryexporter/sentry_exporter.go
@@ -16,10 +16,39 @@ package sentryexporter
 
 import (
 	"context"
+	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/open-telemetry/opentelemetry-collector/component"
 	"github.com/open-telemetry/opentelemetry-collector/consumer/pdata"
 	"github.com/open-telemetry/opentelemetry-collector/exporter/exporterhelper"
+	"github.com/open-telemetry/opentelemetry-collector/translator/conventions"
+)
+
+var (
+	sentryStatusUnknown = "unknown"
+	// canonicalCodes maps OpenTelemetry span codes to Sentry's span status.
+	// See numeric codes in https://godoc.org/github.com/open-telemetry/opentelemetry-proto/gen/go/trace/v1#Status_StatusCode.
+	canonicalCodes = [...]string{
+		"ok",
+		"cancelled",
+		sentryStatusUnknown,
+		"invalid_argument",
+		"deadline_exceeded",
+		"not_found",
+		"already_exists",
+		"permission_denied",
+		"resource_exhausted",
+		"failed_precondition",
+		"aborted",
+		"out_of_range",
+		"unimplemented",
+		"internal",
+		"unavailable",
+		"data_loss",
+		"unauthenticated",
+	}
 )
 
 // SentryExporter defines the Sentry Exporter.
@@ -41,4 +70,147 @@ func CreateSentryExporter(config *Config) (component.TraceExporter, error) {
 	exp, err := exporterhelper.NewTraceExporter(config, s.pushTraceData)
 
 	return exp, err
+}
+
+// TODO: Span.Link
+// TODO; Span.Event -> create breadcrumbs
+// TODO: Span.TraceState()
+func spanToSentrySpan(span pdata.Span) (sentrySpan *SentrySpan) {
+	if span.IsNil() {
+		return nil
+	}
+
+	parentSpanID := ""
+	if psID := span.ParentSpanID(); !isAllZero(psID) {
+		parentSpanID = psID.String()
+	}
+
+	attributes := span.Attributes()
+	name := span.Name()
+	spanKind := span.Kind()
+
+	op, description := generateSpanDescriptors(name, attributes, spanKind)
+	tags := generateTagsFromAttributes(attributes)
+
+	status, message := statusFromSpanStatus(span.Status())
+
+	if message != "" {
+		tags["status_message"] = message
+	}
+
+	if spanKind != pdata.SpanKindUNSPECIFIED {
+		tags["span_kind"] = spanKind.String()
+	}
+
+	return &SentrySpan{
+		TraceID:        span.TraceID().String(),
+		SpanID:         span.SpanID().String(),
+		ParentSpanID:   parentSpanID,
+		Description:    description,
+		Op:             op,
+		Tags:           tags,
+		StartTimestamp: unixNanoToTime(span.StartTime()),
+		EndTimestamp:   unixNanoToTime(span.EndTime()),
+		Status:         status,
+	}
+}
+
+// To generate span descriptors (op and description) for a particular span we use
+// Semantic Conventions described by the open telemetry specification.
+// https://github.com/open-telemetry/opentelemetry-specification/tree/master/specification/trace/semantic_conventions
+func generateSpanDescriptors(name string, attrs pdata.AttributeMap, spanKind pdata.SpanKind) (op string, description string) {
+	var opBuilder strings.Builder
+	var dBuilder strings.Builder
+
+	// Generating span descriptors operates under the assumption that only one of the conventions are present.
+	// In the possible case that multiple convention attributes are available, conventions are selected based
+	// on what is most likely and what is most useful (ex. http is prioritized over FaaS)
+
+	// If http.method exists, this is an http request span.
+	if httpMethod, ok := attrs.Get(conventions.AttributeHTTPMethod); ok {
+		opBuilder.WriteString("http")
+
+		switch spanKind {
+		case pdata.SpanKindCLIENT:
+			opBuilder.WriteString(".client")
+		case pdata.SpanKindSERVER:
+			opBuilder.WriteString(".server")
+		}
+
+		// Ex. description="GET /api/users/{user_id}".
+		fmt.Fprintf(&dBuilder, "%s %s", httpMethod.StringVal(), name)
+
+		return opBuilder.String(), dBuilder.String()
+	}
+
+	// If db.type exists then this is a database call span.
+	if _, ok := attrs.Get(conventions.AttributeDBType); ok {
+		// TODO: Use more detailed op code?
+		opBuilder.WriteString("db")
+
+		// Use DB statement (Ex "SELECT * FROM table") if possible as description.
+		if statement, okInst := attrs.Get(conventions.AttributeDBStatement); okInst {
+			dBuilder.WriteString(statement.StringVal())
+		} else {
+			dBuilder.WriteString(name)
+		}
+
+		return opBuilder.String(), dBuilder.String()
+	}
+
+	// If rpc.service exists then this is a rpc call span.
+	if _, ok := attrs.Get(conventions.AttributeRPCService); ok {
+		opBuilder.WriteString("rpc")
+
+		return opBuilder.String(), name
+	}
+
+	// If messaging.system exists then this is a messaging system span.
+	if _, ok := attrs.Get("messaging.system"); ok {
+		opBuilder.WriteString("message")
+
+		return opBuilder.String(), name
+	}
+
+	// If faas.trigger exists then this is a function as a service span.
+	if trigger, ok := attrs.Get("faas.trigger"); ok {
+		opBuilder.WriteString(trigger.StringVal())
+
+		return opBuilder.String(), name
+	}
+
+	// Default just use span.name.
+	return "", name
+}
+
+func generateTagsFromAttributes(attrs pdata.AttributeMap) Tags {
+	tags := make(map[string]string)
+
+	attrs.ForEach(func(key string, attr pdata.AttributeValue) {
+		switch attr.Type() {
+		case pdata.AttributeValueSTRING:
+			tags[key] = attr.StringVal()
+		case pdata.AttributeValueBOOL:
+			tags[key] = strconv.FormatBool(attr.BoolVal())
+		case pdata.AttributeValueDOUBLE:
+			tags[key] = strconv.FormatFloat(attr.DoubleVal(), 'g', -1, 64)
+		case pdata.AttributeValueINT:
+			tags[key] = strconv.FormatInt(attr.IntVal(), 10)
+		}
+	})
+
+	return tags
+}
+
+func statusFromSpanStatus(spanStatus pdata.SpanStatus) (status string, message string) {
+	if spanStatus.IsNil() {
+		return "", ""
+	}
+
+	code := spanStatus.Code()
+	if code < 0 || int(code) >= len(canonicalCodes) {
+		return sentryStatusUnknown, fmt.Sprintf("error code %d", code)
+	}
+
+	return canonicalCodes[code], spanStatus.Message()
 }
