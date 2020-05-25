69d75f20bf7b75f71a5149ce6c184d6c3aef24d2
Abhijeet Prasad <aprasad@sentry.io>
2020-05-25 15:12:21 -0400

    test: Test async buffer transport

--- exporter/sentryexporter/transport_test.go
+++ exporter/sentryexporter/transport_test.go
@@ -16,12 +16,41 @@ package sentryexporter
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 )
+
+// A testHTTPServer counts events sent to it. It requires a call to Unblock
+// before incrementing its internal counter and sending a response to the HTTP
+// client. This allows for coordinating the execution flow when needed.
+type testHTTPServer struct {
+	*httptest.Server
+	// transactionCounter counts the number of events processed by the server.
+	transactionCounter *uint64
+}
+
+func (ts *testHTTPServer) TransactionCount() uint64 {
+	return atomic.LoadUint64(ts.transactionCounter)
+}
+
+func newTestSentryServer(t *testing.T) *testHTTPServer {
+	transactionCounter := new(uint64)
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		atomic.AddUint64(transactionCounter, 1)
+	}
+
+	return &testHTTPServer{
+		Server:             httptest.NewTLSServer(http.HandlerFunc(handler)),
+		transactionCounter: transactionCounter,
+	}
+}
 
 func TestTransactionEnvelope(t *testing.T) {
 	env, err := transactionToEnvelope(transaction1)
@@ -45,4 +74,104 @@ func TestTransactionEnvelope(t *testing.T) {
 	// Item Payload
 	payload := envParts[2]
 	assert.NotEmpty(t, payload)
+}
+
+func TestNewSentryTransport(t *testing.T) {
+	transport := NewSentryTransport()
+	assert.Equal(t, defaultBufferSize, transport.BufferSize)
+	assert.Equal(t, defaultTimeout, transport.Timeout)
+}
+
+func TestConfigure(t *testing.T) {
+	cfg := &Config{
+		DSN: "https://key@host/path/42",
+	}
+
+	transport := NewSentryTransport()
+	transport.Configure(cfg)
+
+	assert.Equal(t, cfg.DSN, transport.DSN.String())
+	assert.Equal(t, cap(transport.buffer), transport.BufferSize)
+
+	assert.Equal(t, transport.Timeout, transport.client.Timeout)
+	assert.Equal(t, transport.transport, transport.client.Transport)
+}
+
+func TestFlush(t *testing.T) {
+	server := newTestSentryServer(t)
+	defer server.Close()
+
+	cfg := &Config{
+		DSN: fmt.Sprintf("https://test@%s/1", server.Listener.Addr()),
+	}
+
+	t.Run("with flushed events", func(t *testing.T) {
+		transport := NewSentryTransport()
+		transport.Configure(cfg)
+		transport.client = server.Client()
+
+		ok := transport.Flush(100 * time.Millisecond)
+		if !ok {
+			t.Fatalf("Flush() timed out")
+		}
+	})
+
+	t.Run("with timeout", func(t *testing.T) {
+		transport := NewSentryTransport()
+		transport.Configure(cfg)
+		transport.client = server.Client()
+		transport.wg.Add(10000)
+
+		ok := transport.Flush(1)
+		if ok {
+			t.Fatalf("Flush() did not timeout")
+		}
+	})
+}
+
+func TestRetryAfter(t *testing.T) {
+	testCases := []struct {
+		testName string
+		// input
+		now      time.Time
+		response *http.Response
+		// output
+		duration time.Duration
+	}{
+		{
+			testName: "No Header",
+			now:      time.Now(),
+			response: &http.Response{},
+			duration: time.Second * 60,
+		},
+		{
+			testName: "Incorrect Header",
+			now:      time.Now(),
+			response: &http.Response{
+				Header: map[string][]string{
+					"Return-After": {"x"},
+				},
+			},
+			duration: time.Second * 60,
+		},
+		{
+			testName: "Delay Header",
+			now: func() time.Time {
+				now, _ := time.Parse(time.RFC1123, "Wed, 21 Oct 2015 07:28:00 GMT")
+				return now
+			}(),
+			response: &http.Response{
+				Header: map[string][]string{
+					"Retry-After": {"Wed, 21 Oct 2015 07:28:13 GMT"},
+				},
+			},
+			duration: time.Second * 13,
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.testName, func(t *testing.T) {
+			assert.Equal(t, retryAfter(test.now, test.response), test.duration)
+		})
+	}
 }
